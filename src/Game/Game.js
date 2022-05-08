0293c6b325cb4fe1a405d7c326fc095a73a5a456
harper-liang <1960511611@qq.com>
2022-05-09 07:45:26 +0800

    .

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,59 +1,35 @@
-// 进度：历史记录，回跳
+// 在游戏历史记录列表显示每一步棋的坐标，格式为 (列号, 行号)。
+// 在历史记录列表中加粗显示当前选择的项目。
+// 使用两个循环来渲染出棋盘的格子，而不是在代码里写死（hardcode）。
+// 添加一个可以升序或降序显示历史记录的按钮。
+// 每当有人获胜时，高亮显示连成一线的 3 颗棋子。
 import React from "react"
 import './Game.css'
-// 棋盘
-// 轮流下棋
-// 判断谁赢
-// 记录进程，并能返回查看
 function calculateWinner(squares, length, count) {
-  const arrX = []
-  const arrO = []
-  squares.map((item, index) => {
-    if (item === 'O') {
-      arrO.push(index)
-    } else if (item === 'X') {
-      arrX.push(index)
-    }
-  })
-  arrO.sort((a, b) => a - b)
-  arrX.sort((a, b) => a - b)
-  let flags = [length - 1, length, length + 1, 1]
-  if (arrO.length + arrX.length < count * 2 - 1) {
+  const squaresLen = squares.filter( item => item==='X'|| item==='O').length
+  if( squaresLen < (count *2 -1)){
     return null
-  } else {
-    if (arrX.length > arrO.length) {
-      for (let i = 0; i < arrX.length; i++) {
-        let items = []
-        let test = []
-        let z = i
-        // i与所有数相减，找出两个值等于flags内任意一个
-        for (let j = 0; j < arrX.length; j++,z++){
-          test.push([arrX[z],arrX[j]])
-          console.log("test",test)
-          items.push(Math.abs(arrX[z] - arrX[j]))
-        }
-        for(let k = 0; k < flags.length; k++){
-          if(items.filter((item)=>item==flags[k]).length == count-1){
-            return 'X'
-          }
-        }
+  }else{
+    for (let i = 0; i<squares.length; i++) {
+      const x = squares
+      let horizontalArr = []
+      let verticalArr = []
+      let diagonalArr = []
+      for (let j = 0; j < count; j++) {
+        horizontalArr.push(x[i+j])
+        verticalArr.push(x[i+j*length])
+        diagonalArr.push(x[i+j*(length+1)])
       }
-    } else {
-      for (let i = 0; i < arrO.length; i++) {
-      let items = []
-      let z = i
-        for (let j = 0; j < arrO.length; j++, z++){
-          items.push(Math.abs(arrO[z] - arrO[j]))
-        }
-        for(let k = 0; k < flags.length; k++){
-          if(items.filter((item)=>item==flags[k]).length == count-1 ){
-            return 'O'
-          }
-        }
+      if((horizontalArr.length === count && horizontalArr.every(item => item === 'X')) || (verticalArr.length === count && verticalArr.every(item => item === 'X') )|| (diagonalArr.length === count && diagonalArr.every(item => item === 'X'))){
+        return 'X'
+      } else if((horizontalArr.length === count && horizontalArr.every(item => item === 'O')) || (verticalArr.length === count && verticalArr.every(item => item === 'O') )|| (diagonalArr.length === count && diagonalArr.every(item => item === 'O'))){
+        return 'O'
+      } else if(squaresLen === Math.pow(length,2)){
+        return 'D'
       }
     }
-
   }
+  
 }
 function Square(props) {
   return (<div className="square" onClick={props.onClick}>{props.value}</div>)
@@ -87,7 +63,7 @@ function SelModel(props) {
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.length = 10 // 表格长度
+    this.length = 5 // 表格长度
     this.model = "三连棋"
     this.isDisable = false
     this.state = {
@@ -117,7 +93,8 @@ class Game extends React.Component {
     this.setState({
       history: [{ squares: Array(Math.pow(3, 2)).fill(null) }],
       xIsNext: true,
-      count: i
+      count: i,
+      stepFlag: 0
     })
     this.model = i === 3 ? '三连棋' : (i === 4 ? '四连棋' : '五连棋')
     // this.isDisable = true
@@ -125,7 +102,7 @@ class Game extends React.Component {
   jumpTo = (step) => {
     this.setState({
       stepFlag: step,
-      xIsNext: step % 2 == 0
+      xIsNext: step % 2 === 0
     })
   }
   reset = () => {
@@ -143,7 +120,9 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares, this.length, this.state.count)
     let isInit = new Set(current.squares);
     let status;
-    if (winner) {
+    if (winner === 'D') {
+      status = "Double Win"
+    } else if(winner){
       status = 'Winner: ' + winner
     } else {
       if (isInit.size === 1 && isInit.has(null)) {
@@ -152,7 +131,6 @@ class Game extends React.Component {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
       }
     }
-    console.log('winner',winner)
     let moves = history.map((item, step) => {
       const desc = step === 0 ? "go to init" : `go to step ${step}`
       return (<div key={step}><button className="btn" onClick={() => this.jumpTo(step)} disabled={winner ? true : false}>{desc}</button></div>)
--- src/Game/GameStep2.js
+++ src/Game/GameStep2.js
@@ -1,49 +1,32 @@
-// 能够判定玩家何时获胜
-// 能够记录游戏进程
+// 允许玩家查看游戏的历史记录，也可以查看任意一个历史版本的游戏棋盘状态
+// 当无人获胜时，显示一个平局的消息。
 import React from "react"
 import './Game.css'
-function calculateWinner(squares, count) {
-  const arrX = []
-  const arrO = []
-  squares.map((item, index) => {
-    if (item === 'O') {
-      arrO.push(index)
-    } else if (item === 'X') {
-      arrX.push(index)
-    }
-  })
-  arrO.sort((a, b) => a - b)
-  arrX.sort((a, b) => a - b)
-  if (arrO.length + arrX.length < count * 2 - 1) {
+function calculateWinner(squares, length, count) {
+  const squaresLen = squares.filter( item => item=='X'|| item=='O').length
+  if( squaresLen < (count *2 -1)){
     return null
-  } else {
-    let stop = ''
-    // 排序后，四个数之间的差同时等于length/length-1/length+1/1
-    if (arrX.length > arrO.length) {
-      for (let i = 0; i < arrX.length - 1; i++) {
-        const itemX = []
-        for (let c = 0; c < count - 1; c++) {
-          itemX.push(arrX[c + 1] - arrX[c])
-        }
-        if ((new Set(itemX)).size === 1) {
-          stop = 'X'
-          return stop
-        }
+  }else{
+    for (let i = 0; i<squares.length; i++) {
+      const x = squares
+      let horizontalArr = []
+      let verticalArr = []
+      let diagonalArr = []
+      for (let j = 0; j < count; j++) {
+        horizontalArr.push(x[i+j])
+        verticalArr.push(x[i+j*length])
+        diagonalArr.push(x[i+j*(length+1)])
       }
-    } else {
-      for (let i = 0; i < arrO.length; i++) {
-        const itemO = []
-        for (let c = 0; c < count - 1; c++) {
-          itemO.push(arrO[c + 1] - arrO[c])
-        }
-        if ((new Set(itemO)).size === 1) {
-          stop = 'O'
-          return stop
-        }
+      if((horizontalArr.length == count && horizontalArr.every(item => item == 'X')) || (verticalArr.length == count && verticalArr.every(item => item == 'X') )|| (diagonalArr.length == count && diagonalArr.every(item => item == 'X'))){
+        return 'X'
+      } else if((horizontalArr.length == count && horizontalArr.every(item => item == 'O')) || (verticalArr.length == count && verticalArr.every(item => item == 'O') )|| (diagonalArr.length == count && diagonalArr.every(item => item == 'O'))){
+        return 'O'
+      } else if(squaresLen == Math.pow(length,2)){
+        return 'D'
       }
     }
-
   }
+  
 }
 function Square(props) {
   return (<div className="square" onClick={props.onClick}>{props.value}</div>)
@@ -70,84 +53,101 @@ function SelModel(props) {
     { model: '五连棋', count: 5, isSel: false },
   ]
   const element = modelArr.map((item) =>
-    <button className="modelBtn" key={item.count} onClick={() => props.onClick(item.count)}>{item.model}</button>
+    <button className="btn" key={item.count} onClick={() => props.onClick(item.count)}>{item.model}</button>
   )
   return (element)
 }
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.length = 10 // 表格长度
+    this.length = 5 // 表格长度
     this.model = "三连棋"
     this.isDisable = false
     this.state = {
       history: [{ squares: Array(Math.pow(3, 2)).fill(null) }],
       xIsNext: true,
-      count: 3 // 几字棋子
+      count: 3, // 几连棋子
+      stepFlag: 0,
     }
   }
   handleClick = (i) => {
-    const history = this.state.history.slice()
+    const history = this.state.history.slice(0, this.state.stepFlag + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
-    if (calculateWinner(current.squares, this.state.count) || squares[i]) {
+    if (calculateWinner(current.squares, this.length, this.state.count) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
-      history: this.state.history.concat([{
+      history: history.concat([{
         squares
       }]),
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      stepFlag: history.length
     })
   }
   manageBtn = (i) => {
     this.setState({
       history: [{ squares: Array(Math.pow(3, 2)).fill(null) }],
       xIsNext: true,
-      count: i
+      count: i,
+      stepFlag: 0
     })
     this.model = i === 3 ? '三连棋' : (i === 4 ? '四连棋' : '五连棋')
     // this.isDisable = true
+  }
+  jumpTo = (step) => {
+    this.setState({
+      stepFlag: step,
+      xIsNext: step % 2 == 0
+    })
   }
   reset = () => {
     const count = this.state.count
     this.setState({
       history: [{ squares: Array(Math.pow(3, 2)).fill(null) }],
       xIsNext: true,
-      count: count
+      count: count,
+      stepFlag: 0
     })
   }
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
-    const winner = calculateWinner(current.squares, this.state.count)
+    const current = history[this.state.stepFlag]
+    const winner = calculateWinner(current.squares, this.length, this.state.count)
     let isInit = new Set(current.squares);
     let status;
-    if (winner) {
+    if (winner == 'D') {
+      status = "Double Win"
+    } else if(winner){
       status = 'Winner: ' + winner
     } else {
-      if(isInit.size === 1 && isInit.has(null)){
+      if (isInit.size === 1 && isInit.has(null)) {
         status = 'please start the game!'
-      }else{
+      } else {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
       }
     }
+    let moves = history.map((item, step) => {
+      const desc = step === 0 ? "go to init" : `go to step ${step}`
+      return (<div key={step}><button className="btn" onClick={() => this.jumpTo(step)} disabled={winner ? true : false}>{desc}</button></div>)
+    })
     return (
       <div className="game-box">
         <div className="game-left">
           <div className="select-box">
-            <h2 style={{color: '#fbc847'}}>{status}</h2>
+            <h2 style={{ color: '#fbc847' }}>{status}</h2>
             <p>当前模式：{this.model}</p>
             切换模式：<SelModel onClick={(i) => this.manageBtn(i)}></SelModel>
             {/* <button>选择棋盘大小<button></button><button></button><button></button></button> */}
-            <button className="modelBtn" onClick={this.reset}>重新开始</button>
+            <button className="btn" onClick={this.reset}>重置</button>
           </div>
           <Board count={this.length} squares={current.squares} onClick={(i) => this.handleClick(i)}></Board>
         </div>
         <div className="game-right">
           <div className="tip-box">
             <div>历史记录</div>
+            <div>{moves}</div>
           </div>
         </div>
 
